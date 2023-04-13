cf072adfbb9261255e4d8af94a7863b19fedbd00
Jen <107281506+jenvdr@users.noreply.github.com>
2023-04-13 11:43:27 +0200

    Fix indendation

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,40 +1,39 @@
 import { useState, useCallback } from "react";
 
 const useHTTP = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-  
-    const sendRequest = useCallback(async (requestConfig, applyData) => {
-        setIsLoading(true);
-        setError(null);
-        try {
-            const response = await fetch(
-                requestConfig.url, {
-                    method: requestConfig.method ? requestConfig.method : 'GET',
-                    headers: requestConfig.headers ? requestConfig.headers : {},
-                    body: requestConfig.body ? JSON.stringify(requestConfig.body) : null
-                }
-            );
-    
-            if (!response.ok) {
-            throw new Error('Request failed!');
-            }
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-            const data = await response.json();
-            applyData(data);
-
-        } catch (err) {
-            setError(err.message || 'Something went wrong!');
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        requestConfig.url, {
+          method: requestConfig.method ? requestConfig.method : 'GET',
+          headers: requestConfig.headers ? requestConfig.headers : {},
+          body: requestConfig.body ? JSON.stringify(requestConfig.body) : null
         }
-            setIsLoading(false);
-        }, []);
+      );
 
-        return {
-            isLoading,
-            error,
-            sendRequest: sendRequest
-        }
-        
-    };
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
 
-    export default useHTTP; 
+      const data = await response.json();
+      applyData(data);
+
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+      setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendRequest: sendRequest
+  }
+};
+
+export default useHTTP; 
