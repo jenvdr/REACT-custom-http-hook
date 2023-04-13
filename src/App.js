5e73f3724200216b6016c0eea00ad0591b47f56c
Jen <107281506+jenvdr@users.noreply.github.com>
2023-04-13 10:49:27 +0200

    Restructure and make use of custom useHTTP hook

--- src/App.js
+++ src/App.js
@@ -2,39 +2,24 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
-import useHTTP from './hooks/http-hook';
+import useHTTP from './hooks/use-http';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://custom-http-hook-f524f-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'
-      );
+  const transformData = data => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    for (const taskKey in data) {
+      loadedTasks.push({ id: taskKey, text: data[taskKey].text });
     }
-    setIsLoading(false);
-  };
+
+    setTasks(loadedTasks);
+  }
+
+  const {isLoading, error, sendRequest: fetchTasks} = useHTTP({
+    url: 'https://custom-http-hook-f524f-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'
+  }, transformData);
 
   useEffect(() => {
     fetchTasks();
