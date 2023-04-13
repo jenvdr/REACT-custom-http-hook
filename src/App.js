793c6cc191e9ce85fc8f559c48eeafc664424b3f
Jen <107281506+jenvdr@users.noreply.github.com>
2023-04-13 11:46:00 +0200

    Remove whitespace and fix indentation

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import React, { useEffect, useState, useCallback } from 'react';
-
+import React, { useEffect, useState} from 'react';
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
 import useHTTP from './hooks/use-http';
 
 function App() {
   const [tasks, setTasks] = useState([]);
-
   const {isLoading, error, sendRequest: fetchTasks} = useHTTP();
 
   useEffect(() => {
     const transformData = (data => {
-        const loadedTasks = [];
-    
-        for (const taskKey in data) {
-          loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-        }
-    
-        setTasks(loadedTasks);
-      });
+      const loadedTasks = [];
+
+      for (const taskKey in data) {
+        loadedTasks.push({id: taskKey, text: data[taskKey].text});
+      }
+
+      setTasks(loadedTasks);
+    });
 
     fetchTasks({
-        url: 'https://custom-http-hook-f524f-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'
+      url: 'https://custom-http-hook-f524f-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'
     }, transformData);
   }, [fetchTasks]);
 
-  const taskAddHandler = (task) => {
+  const taskAddHandler = task => {
     setTasks((prevTasks) => prevTasks.concat(task));
   };
 
