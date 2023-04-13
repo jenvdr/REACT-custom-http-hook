8905e451f8b22e057d495a81c3c861fddfa8c9b2
Jen <107281506+jenvdr@users.noreply.github.com>
2023-04-13 10:22:27 +0200

    Set up first structure for custom hook based on get request in App.js

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,7 +1,40 @@
 import { useState, useEffect } from "react";
 
-const useHTTP = () => {
+const useHTTP = (requestConfig, applyData) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+  
+    const sendRequest = async (taskText) => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch(
+                requestConfig.url, {
+                    method: requestConfig.method,
+                    headers: requestConfig.headers,
+                    body: JSON.stringify(requestConfig.body)
+                }
+            );
+    
+            if (!response.ok) {
+            throw new Error('Request failed!');
+            }
 
-};
+            const data = await response.json();
+            applyData(data);
 
-export default useHTTP;
+        } catch (err) {
+            setError(err.message || 'Something went wrong!');
+        }
+            setIsLoading(false);
+        };
+
+        return {
+            isLoading,
+            error,
+            sendRequest: sendRequest
+        }
+        
+    };
+
+    export default useHTTP; 
